b6da7f90bc9a8444141870561709e9f07141fe8e
Gu <effy@im007.com>
2020-06-24 09:16:20 +0800

    无符号编译,减少编译文件体积

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ set GOOS=linux
 if  "%1"== "" (set FILE_NAME=) else set FILE_NAME= -o %1
 echo %cd%
 echo 正在编译 go build %cd%%FILE_NAME%
-go build%FILE_NAME%&& echo 编译成功 && goto OK
+go build -ldflags "-s -w"%FILE_NAME%&& echo 编译成功 && goto OK
 echo 编译失败
 :OK
